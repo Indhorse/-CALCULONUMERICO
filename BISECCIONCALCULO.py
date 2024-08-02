8b88bdac2f6660c32041f99a6cbd176b05754cf6
Indhorse <125569780+Indhorse@users.noreply.github.com>
2024-08-01 22:29:59 -0400

    Update BISECCIONCALCULO.py

--- BISECCIONCALCULO.py
+++ BISECCIONCALCULO.py
@@ -1,19 +1,7 @@
 import math
 
 def biseccion(f, a, b, tol=1e-5, max_iter=100):
-    """
-    Método de bisección para encontrar raíces de una función.
-
-    Parámetros:
-    f (función): función a la que se le busca la raíz
-    a (float): límite inferior del intervalo
-    b (float): límite superior del intervalo
-    tol (float, opcional): tolerancia para la aproximación (default: 1e-5)
-    max_iter (int, opcional): número máximo de iteraciones (default: 100)
-
-    Retorna:
-    float: aproximación de la raíz
-    """
+  
     if f(a) * f(b) > 0:
         raise ValueError("La función no cambia de signo en el intervalo")
 
